7d9d5c9761f7cf03fa964ca4ee0bdf4485cab644
rasathelamedude <rasyar.safen19@gmail.com>
2026-05-12 20:09:40 +0300

    Implement notifier as another kafka consumer

--- with-kafka/services/notifier/index.ts
+++ with-kafka/services/notifier/index.ts
@@ -1,1 +1,36 @@
-console.log('Notifier Service is running...');
+import { Kafka } from "kafkajs";
+import { Transaction } from "../../types/Transaction";
+
+const kafka = new Kafka({
+  clientId: "wireflow-notifier",
+  brokers: ["localhost:29092"],
+});
+
+const consumer = kafka.consumer({ groupId: "notifier-group" });
+
+const runConsumer = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: "transactions", fromBeginning: true });
+
+  consumer.run({
+    eachMessage: async ({ partition, message, topic }) => {
+      try {
+        const value = message.value?.toString();
+
+        if (!value) return;
+
+        const transaction: Transaction = JSON.parse(value);
+
+        console.log(`Processing transaction ${transaction.transactionId}...`);
+      } catch (error) {
+        console.log(`Error processing transaction: ${error}`);
+      }
+    },
+  });
+};
+
+async function main() {
+  await runConsumer();
+}
+
+main();
